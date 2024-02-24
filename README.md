571cc7cba97930afd30b4b42eb1de6977e7023c0
Matin Schams <schams.matin@gmail.com>
2024-02-24 02:42:18 -0800

    Integrate Reddit API and filter through post titles and body (color WIP)

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # ML-Sentiment-Analyzer
 
-Analyze sentiment of text data applying natural language processing techniques
-and machine learning algorithms to classify text into categories of positive,
-negative or neutral tone.
+Analyze sentiment of text data by applying natural language processing
+techniques to classify text into categories of positive, negative or neutral
+sentiment.
 
 # TextBlob: Processing Textual Data
 
@@ -14,4 +14,16 @@ part-of-speech tagging, sentiment analysis, and text classification.
 
 <div style="text-align:center">
   <img src="https://i.imgur.com/4KkO5UG.gif" alt="Image description" />
+</div>
+
+# How it works
+
+1. Type in any subreddit into the field
+2. Type in a specific keyword you want to filter the posts by
+3. Get a sentiment score back, telling you the overall sentiment of the posts
+
+# Demo with Reddit API
+
+<div style="text-align:center">
+  <img src="https://i.imgur.com/5vtQqZ6.gif" alt="Image description" />
 </div>
--- app.py
+++ app.py
@@ -1,32 +1,63 @@
 from flask import Flask, render_template, request
 from textblob import TextBlob
+import praw
 
 app = Flask(__name__)
+
+def authenticate_reddit():
+    reddit = praw.Reddit(
+        client_id='uEpj3XpBBXxwn_Evc_nDHA',
+        client_secret='tfaBTItElM2lJwFF1ETDBVEzjUjB3w',
+        user_agent='Sentiment Analysis by /u/matinbean'
+    )
+    return reddit
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+def get_reddit_posts(subreddit, keyword):
+    reddit = authenticate_reddit()
+    posts = reddit.subreddit(subreddit).search(keyword)
+    return posts
+
+def analyze_sentiment(posts):
+    sentiment_scores = []
+    for post in posts:
+        title_blob = TextBlob(post.title)
+        title_sentiment = title_blob.sentiment.polarity
+
+        body_blob = TextBlob(post.selftext)
+        body_sentiment = body_blob.sentiment.polarity
+
+        average_sentiment = (title_sentiment + body_sentiment) / 2
+        sentiment_scores.append(average_sentiment)
+
+    if sentiment_scores:
+        average_sentiment = sum(sentiment_scores) / len(sentiment_scores)
+    else:
+        average_sentiment = 0
+    
+    return average_sentiment
+
+
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    text = request.form['text']
-    blob = TextBlob(text)
-    sentiment = blob.sentiment
-
-    polarity = sentiment.polarity
-    subjectivity = sentiment.subjectivity
-
-    if polarity > 0:
+    subreddit = request.form['subreddit']
+    keyword = request.form['keyword']
+    posts = get_reddit_posts(subreddit, keyword)
+    average_sentiment = analyze_sentiment(posts)
+    
+    if average_sentiment > 0:
         sentiment_label = 'Positive'
         color = 'green'
-    elif polarity < 0:
+    elif average_sentiment < 0:
         sentiment_label = 'Negative'
         color = 'red'
     else:
         sentiment_label = 'Neutral'
-        color = 'black'
-
-    return render_template('result.html', text=text, polarity=polarity, subjectivity=subjectivity, sentiment_label=sentiment_label, color=color)
+    
+    return render_template('result.html', subreddit=subreddit, keyword=keyword, sentiment_label=sentiment_label, average_sentiment=average_sentiment, color=color)
 
 if __name__ == '__main__':
     app.run(debug=True)
